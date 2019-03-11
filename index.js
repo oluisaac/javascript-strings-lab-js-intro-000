2c02f4f1aef8692ddaa960a22f51076b32e07d74
Olu Isaac <oluisaac7@gmail.com>
2019-03-11 20:02:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var greeting = "Hello, everybody!";
 
-var specialGuest = "Hello, Neil deGrasse Tyson"
+var specialGuest = "Hello, everybody! Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, everybody!" + specialGuest + "!";
 
